6461a68b2c6ccd58dd75c456b08377517833fc08
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-07 11:20:53 +0300

    Refactor project structure: update static file paths, remove unused assets, and add new CSS styles for improved layout and button design

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 5500;
 
 app.set("view engine", "ejs");
 
-app.use("/public", express.static("src"));
+app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
