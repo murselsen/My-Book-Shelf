69addd2d28ee731e9272f5a1099dd5d1d2377077
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-12 23:46:23 +0300

    fix(home): Log book data instead of entire object

--- controllers/books.js
+++ controllers/books.js
@@ -1,16 +1,26 @@
 const axios = require('axios');
 
-const topBooks = async () => {
+const topBooks = () => {
   // https://books-backend.p.goit.global/books/top-books
-  const response = await axios.get(
-    'https://books-backend.p.goit.global/books/top-books'
-  );
-    return response.data;
-    
 
-    return new Promise((resolve, reject) => {
-        
-    });
+  return new Promise((resolve, reject) => {
+    axios
+      .get('https://books-backend.p.goit.global/books/top-books')
+      .then(response => {
+        resolve({
+          data: response.data,
+          status: response.status,
+          statusText: response.statusText,
+          headers: response.headers,
+          config: response.config,
+          request: response.request,
+          response: response,
+        });
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
 };
 
 module.exports = {
--- routers/main.js
+++ routers/main.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { topBooks } = require('../controllers/books');
 router.get('/home', async (req, res) => {
   const books = await topBooks();
-  console.log(books);
+  console.log(books.data);
   res.render('home');
 });
 
