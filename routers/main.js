213fd75da92c06a5bdf3ff9075e22d1ce5dc1a0d
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-12 23:49:59 +0300

    fix(home): close loop properly in home view This commit adds the missing `<% }) %>` tag within the book item loop in the home view, fixing an issue where the loop was not being properly closed.

--- routers/main.js
+++ routers/main.js
@@ -5,7 +5,7 @@ const { topBooks } = require('../controllers/books');
 router.get('/home', async (req, res) => {
   const books = await topBooks();
   console.log(books.data);
-  res.render('home');
+  res.render('home', { books: books.data });
 });
 
 module.exports = router;
